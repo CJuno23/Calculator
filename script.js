265b6fa0bf01152229d20a19ea9de1279ee9993a
juno <juno45381@gmail.com>
2025-09-19 17:24:55 +0800

    changes when num1 divided by 0

--- script.js
+++ script.js
@@ -142,7 +142,11 @@ function multiplication(num1,num2){
     return num1*num2;
 }
 function division(num1,num2){
-    return num1/num2;
+    if (num2 === 0) {
+        alert("not divisible by 0")
+    }else {
+        return num1/num2;
+    }
 }
 function operate(operator,num1,num2) {
     if (operator === "+") {
